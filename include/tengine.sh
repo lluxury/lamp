a436a7afa77598f2cdbc9ac4a3f19778f499ccd0
yann <tmp4321@qq.com>
2019-05-02 20:18:57 +0800

    fix nginx boot error

--- include/tengine.sh
+++ include/tengine.sh
@@ -91,9 +91,38 @@ config_tengine(){
     ln -s ${nginx_location}/sbin/nginx /usr/sbin/nginx
     chmod +x /usr/sbin/nginx
 
-    cp -f ${cur_dir}/init.d/nginx-init /etc/init.d/nginx
-    chmod 755 /etc/init.d/nginx
-    chkconfig --add nginx
+    cat > /usr/lib/systemd/system/nginx.service  <<EOF
+[Unit]
+Description=The nginx HTTP and reverse proxy server
+After=network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=forking
+PIDFile=/run/nginx.pid
+# Nginx will fail to start if /run/nginx.pid already exists but has the wrong
+# SELinux context. This might happen when running `nginx -t` from the cmdline.
+# https://bugzilla.redhat.com/show_bug.cgi?id=1268621
+ExecStartPre=/usr/bin/rm -f /run/nginx.pid
+ExecStartPre=/usr/sbin/nginx -t
+ExecStart=/usr/sbin/nginx
+ExecReload=/bin/kill -s HUP $MAINPID
+KillSignal=SIGQUIT
+TimeoutStopSec=5
+KillMode=process
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    # nginx
+# systemctl start nginx.service
+
+chmod 644 /usr/lib/systemd/system/nginx.service
+systemctl enable nginx
+
+    # cp -f ${cur_dir}/init.d/nginx-init /etc/init.d/nginx
+    # chmod 755 /etc/init.d/nginx
+    # chkconfig --add nginx
 
 }
 
